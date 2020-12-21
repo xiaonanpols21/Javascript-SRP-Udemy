3d4704b7a76eb36a461d18b68b8764529e9680c9
xiaonanpols21 <74655427+xiaonanpols21@users.noreply.github.com>
2020-12-21 10:07:43 +0100

    Variables

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,1 +1,11 @@
-console.log('Hello world');
+// console.log('Hello world'); Deel 1: Getting started
+let name = 'Mosh';
+console.log(name);
+
+let firstName;
+let FirstName;
+
+let firstName = 'Xiao Nan';
+let FirstName = 'Pols';
+
+// Camel notation
