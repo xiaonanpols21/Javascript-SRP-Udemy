41018f24835d93a7eff1c372c2f1e7118cb54491
xiaonanpols21 <74655427+xiaonanpols21@users.noreply.github.com>
2020-12-21 11:34:39 +0100

    Enf Part 2

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Dit is mijn Javascript code voor mijn SRP punt van CMD eerste jaar.
 
 js-basic en js-basics downloaded zijn van het deel: Getting Started. Dus niet van deel 2: Basics.
+
+De vele comments in de index.js, zijn omdat ik steeds een nieuwe opdracht doe, maar ik wil wel de vorige code houden. 
--- js-basics/index.js
+++ js-basics/index.js
@@ -45,3 +45,25 @@
 
 // console.log(person.name);
 // End Deel 2, Objects
+
+// Start Deel 2, Arrays
+// let selectedColors = ['red', 'blue'];
+// selectedColors[2] = 1;
+// console.log (selectedColors.length);
+// End Deel 2, Arrays
+
+// Start Deel 2, Functions
+// Performing a task
+// function greet (name, lastName) {
+//     console.log ('Hello ' + name + '' + lastName);
+// }
+
+// // Calculating a value
+// function square(number) {
+//     return number * number;
+// }
+
+// // greet('John', 'Smith');
+
+// console.log(square (2));
+// End Deel 2, Functions
