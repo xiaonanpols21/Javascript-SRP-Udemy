a01d85791d9e68fa8f75cb00d2d8872d2830a7df
xiaonanpols21 <74655427+xiaonanpols21@users.noreply.github.com>
2020-12-21 10:43:48 +0100

    Start deel 2
    Hier ben ik aan het beginnen met het weten wat wat is.

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,11 +1,47 @@
 // console.log('Hello world'); Deel 1: Getting started
-let name = 'Mosh';
-console.log(name);
 
-let firstName;
-let FirstName;
+// Start: Deel 2, Variables
+// let name = 'Mosh';
+// console.log(name);
 
-let firstName = 'Xiao Nan';
-let FirstName = 'Pols';
+// let firstName;
+// let FirstName;
+
+// let firstName = 'Xiao Nan';
+// let FirstName = 'Pols';
 
 // Camel notation
+// End: Deel 2, Variables
+
+// Start Deel 2, Consistend
+// const interestRate = 0.3;
+// interestRate = 1;
+// console.log(interestRate);
+// End Deel 2, Consistend
+
+// Start Deel 2, primitive types
+// let name = 'Mosh'; // String literal
+// let age = 30; // Number literal
+// let isApproved = true; // Boolean literal
+// let = firstName = undefined;
+// let selectedColor = null;
+// End Deel 2, primitive types
+
+// Start Deel 2, Objects
+// let name = 'Mosh';
+// let age = '30';
+
+// let person = { // Object litteral
+//     name: 'Mosh',
+//     age: 30
+// };
+
+// // Dot notation
+// person.name = 'John';
+
+// // Bracket notation
+// let selection = 'name';
+// person[selection] = 'Mary';
+
+// console.log(person.name);
+// End Deel 2, Objects
