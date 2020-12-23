a4b24c8fb1f030adbdb29148769945cbe7034820
xiaonanpols21 <74655427+xiaonanpols21@users.noreply.github.com>
2020-12-23 09:54:39 +0100

    Cursus afgerond
    Hierbij heb ik de cursus afgerond

--- js-basics/index.js
+++ js-basics/index.js
@@ -1081,3 +1081,46 @@
 
 // playVideo();
 // End Deel 7, changing
+
+// --- Start Deel 7, Exercise Sam arguments
+// console.log(sum([1, 2, 3, 4]));
+
+// function sum(...items) { 
+//   if (items.length === 1 && Array.isArray(items[0]))
+//     items = [...items]; 
+
+//   return items.reduce((a, b) => a + b);
+// }
+// End Deel 7, Exercise Sam arguments
+
+// --- Start Deel 7, Exercise Circle
+// const circle = {
+//     radius: 1,
+//     get area() {
+//       return Math.PI * this.radius * this.radius;
+//     }
+//   };
+  
+//   console.log(circle.area);
+// End Deel 7, Exercise Circle
+
+// --- Start Deel 7, Exercise Error handling
+// try {
+//     const numbers = [1, 2, 3, 4]; 
+//     const count = countOccurrences(null, 1); 
+//     console.log(count); 
+//   }
+//   catch (e) {
+//     console.log(e.message);
+//   }
+  
+//   function countOccurrences(array, searchElement) {
+//     if (!Array.isArray(array))
+//       throw new Error('Invalid array.');
+  
+//     return array.reduce((accumulator, current) => {
+//       const occurrence = (current === searchElement) ? 1 : 0;
+//       return accumulator + occurrence;
+//     }, 0);
+// }
+// End Deel 7, Exercise Error handling
